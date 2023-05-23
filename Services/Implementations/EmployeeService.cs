35ebbc8e362729386d826178ab3d2596c247e2a7
Lucas Paiva <lucasp4iva@gmail.com>
2023-05-23 11:18:46 -0300

    feat: add employeeService

---
+++ Services/Implementations/EmployeeService.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeManagement.DTOs;
+using EmployeeManagement.Models;
+using EmployeeManagement.Persistence;
+using EmployeeManagement.Services.Interfaces;
+
+namespace EmployeeManagement.Services.Implementations
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly DataContext _context;
+
+        public EmployeeService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Employee> DismissEmployee(string registerNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetEmployee(string registerNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> RegisterEmployee(EmployeeDTO employeeDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> UpdateEmployee(EmployeeDTO employeeDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Services/Interfaces/IEmployeeService.cs
+++ Services/Interfaces/IEmployeeService.cs
@@ -1,7 +1,13 @@
-﻿namespace EmployeeManagement.Services.Interfaces
+﻿using EmployeeManagement.DTOs;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Services.Interfaces
 {
     public interface IEmployeeService
     {
-
+        Task<Employee> RegisterEmployee(EmployeeDTO employeeDTO);
+        Task<Employee> GetEmployee(string registerNumber);
+        Task<Employee> UpdateEmployee(EmployeeDTO employeeDTO);
+        Task<Employee> DismissEmployee(string registerNumber);
     }
 }
